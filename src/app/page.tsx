8be049849b1bbe84560870fa69ccdb8dd7939d34
Paulo Turino <paulo.turino01@gmail.com>
2023-10-14 11:00:26 -0300

    feat: Add Footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <main className="flex flex-col">
-      <Header/>
-    </main>
+    <>
+     <Header/>
+      <main className="flex flex-col">
+
+      </main>
+      <Footer />
+    </>
+     
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <footer className="bg-[var(--gray)] p-4 text-center">
+            <p className="text-[var(--white)]">© Copyright 2023 - Lorem ipsum dolor sit amet consectetur</p>
+        </footer>
+    )
+}
